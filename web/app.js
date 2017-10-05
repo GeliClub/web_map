d0fc8695c0a02952e7cd092ec54f3e6ff9c11383
Nick Li <hli109@hawk.iit.edu>
2017-10-05 12:53:03 -0500

    added sb0 scaled to sb1 and sb2

--- web/app.js
+++ web/app.js
@@ -1,38 +1,49 @@
 
 var s = Snap(850, 1100);
 
+var sb0 = s.group();
 var sb1 = s.group();
 var sb2 = s.group();
 
-var b0 = s.rect(10,10,50,50,10,10);
-var b1 = s.rect(10,80,50,50,10,10);
-var b2 = s.rect(10,150,50,50,10,10);
+var ba = s.rect(10,10,50,50,10,10);
+var b0 = s.rect(10,110,50,50,10,10);
+var b1 = s.rect(10,210,50,50,10,10);
+var b2 = s.rect(10,310,50,50,10,10);
 
 
 
+Snap.load("assets/SB-00.svg", (data) => {
+	sb0.append(data);
+});
 Snap.load("assets/SB-01.svg", (data) => {
 	sb1.append(data);
 });
-
 Snap.load("assets/SB-02.svg", (data) => {
 	sb2.append(data);
 });
 
 
 
-b0.click(() => {
+ba.click(() => {
+	sb0.attr({visibility: "visible"});
 	sb1.attr({visibility: "visible"});
 	sb2.attr({visibility: "visible"});
+});
+b0.click(() => {
+	if (sb0.attr("visibility") === "visible") 
+		sb0.attr({visibility: "hidden"});
+	else 
+		sb0.attr({visibility: "visible"});
 });
 b1.click(() => {
-	sb1.attr({visibility: "visible"});
-	sb2.attr({visibility: "hidden"});
+	if (sb1.attr("visibility") === "visible") 
+		sb1.attr({visibility: "hidden"});
+	else 
+		sb1.attr({visibility: "visible"});
 });
 b2.click(() => {
-	sb1.attr({visibility: "hidden"});
-	sb2.attr({visibility: "visible"});
+	if (sb2.attr("visibility") === "visible") 
+		sb2.attr({visibility: "hidden"});
+	else 
+		sb2.attr({visibility: "visible"});
 });
-
-
-//var hoverover = function() { g.animate({ transform: 's2r45,150,150' }, 1000, mina.bounce ) };
-//var hoverout = function() { g.animate({ transform: 's1r0,150,150' }, 1000, mina.bounce ) };
