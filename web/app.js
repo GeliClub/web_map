61477862f0870c02bcd1dda19703891b52ea2080
Nick Li <hli109@hawk.iit.edu>
2017-10-06 18:13:28 -0500

    working on database access

--- web/app.js
+++ web/app.js
@@ -1,49 +1,66 @@
 
 var s = Snap(850, 1100);
+var points = []; // user location list
 
-var sb0 = s.group();
-var sb1 = s.group();
-var sb2 = s.group();
+var sb = [s.group(), s.group(), s.group()]; // stuart floors
 
-var ba = s.rect(10,10,50,50,10,10);
-var b0 = s.rect(10,110,50,50,10,10);
-var b1 = s.rect(10,210,50,50,10,10);
-var b2 = s.rect(10,310,50,50,10,10);
+var origin = s.circle(162,721,2);
+origin.attr({visibility: "hidden"});
+
+// buttons
+var ba = [s.rect(10,10,50,50,10,10)];
+var b0 = [s.rect(10,110,50,50,10,10)];
+var b1 = [s.rect(10,210,50,50,10,10)];
+var b2 = [s.rect(10,310,50,50,10,10)];
 
 
-
+// load svg onto different groups
 Snap.load("assets/SB-00.svg", (data) => {
-	sb0.append(data);
+	sb[0].append(data);
+	sb[0].attr({visibility: "hidden"});
 });
 Snap.load("assets/SB-01.svg", (data) => {
-	sb1.append(data);
+	sb[1].append(data);
+	sb[1].attr({visibility: "hidden"});
 });
 Snap.load("assets/SB-02.svg", (data) => {
-	sb2.append(data);
+	sb[2].append(data);
+	//sb[2].attr({visibility: "hidden"});
 });
 
+var scale = 11;
 
+function addPoint(x,y,r,f) {
+	var offset = origin.node.getBBox();
+	if (f > -1 && f < sb.length)
+		points.push(sb[f].circle(offset.x+scale*x,offset.y-scale*y,r));
+	else
+		points.push(s.circle(offset.x+scale*x,offset.y-scale*y,r));
+}
 
-ba.click(() => {
-	sb0.attr({visibility: "visible"});
-	sb1.attr({visibility: "visible"});
-	sb2.attr({visibility: "visible"});
+var text = s.text(180, 50, "Stuart Building");
+text.attr({"font-size": 50, "fill": 'blue'})
+
+ba[0].click(() => {
+	sb[0].attr({visibility: "visible"});
+	sb[1].attr({visibility: "visible"});
+	sb[2].attr({visibility: "visible"});
 });
-b0.click(() => {
-	if (sb0.attr("visibility") === "visible") 
-		sb0.attr({visibility: "hidden"});
+b0[0].click(() => {
+	if (sb[0].attr("visibility") === "visible") 
+		sb[0].attr({visibility: "hidden"});
 	else 
-		sb0.attr({visibility: "visible"});
+ 		sb[0].attr({visibility: "visible"});
 });
-b1.click(() => {
-	if (sb1.attr("visibility") === "visible") 
-		sb1.attr({visibility: "hidden"});
+b1[0].click(() => {
+	if (sb[1].attr("visibility") === "visible") 
+		sb[1].attr({visibility: "hidden"});
 	else 
-		sb1.attr({visibility: "visible"});
+		sb[1].attr({visibility: "visible"});
 });
-b2.click(() => {
-	if (sb2.attr("visibility") === "visible") 
-		sb2.attr({visibility: "hidden"});
+b2[0].click(() => {
+	if (sb[2].attr("visibility") === "visible") 
+		sb[2].attr({visibility: "hidden"});
 	else 
-		sb2.attr({visibility: "visible"});
+		sb[2].attr({visibility: "visible"});
 });
